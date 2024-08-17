93fe9d0932f1f021928ce750991b34e5b3261a0f
sherif2003 <sherif2102003@gmail.com>
2024-08-17 18:46:05 +0300

    Add Painter
    Remove HorizontalMarkerLine
    
    Add CodeLine

--- src/code_reporter/mod.rs
+++ src/code_reporter/mod.rs
@@ -1,22 +1,27 @@
-use std::{collections::HashMap, marker::PhantomData};
+use std::{collections::HashMap, fmt::{self, write, Display}, marker::{self, PhantomData}};
 
-use owo_colors::{AnsiColors::Black, DynColors, Style, };
+use itertools::Itertools;
+use owo_colors::{AnsiColors::Black, DynColors, OwoColorize, Style, Styled };
+use painter::Painter;
 
 use crate::span::Span;
 
+mod painter;
+
 /**
  * let mutable x = 5555 && false;
- *     ^^^     -    ^      ^^^^^ Label 0  (This is the main marker line and it has a label if there is more than one in the code line)
- * ____|_______|____|                     (This is the sub main marker line, and it has no labels)
- * ____|_______|    Label 1               (This is a sub marker line, and it has a label if )
- *     |       |    Label 1               (This is a sub marker line, and it has a label if )
- *     |       Label 2
- *     Label 3
+ *     ^^^     -    ^   ~~ ^^^^^ Label 0   (This is the main marker line and it has a label if there is more than one in the code line)
+ * ____|_______|____|   |                  (This is the sub main marker line, and it has no labels)
+ * ____|_______|    |   Label 1            (This is a sub marker line, and it has a label)
+ *     |       |    Label 2                (~)
+ *     |       Label 3                     (~)
+ *     |       Label 4                     (The label belongs to the same marker)
+ *     Label 5                             (This is a sub marker line, and it has a label)
  */
 
 struct CodeReporter<'a> {
     /// Map lines indecies and main depth line on them
-    lines_to_report: HashMap<usize, HorizontalMarkerLine<'a, MainDepth>>,
+    lines_to_report: HashMap<usize, CodeLine<'a>>,
     /// Lines to read from
     files_lines: &'a [&'a str],
 }
@@ -40,19 +45,19 @@ impl<'a> CodeReporter<'a> {
         if start_line == end_line {
 
             self.lines_to_report.entry(start_line)
-                .or_insert( HorizontalMarkerLine::default() )
-                .mark_in_line(start_col, end_col, sign, style, labels);
+                .or_insert( CodeLine::default() )
+                .mark_as_one_line(start_col, end_col, sign, style, labels);
 
             return self;
         }
 
 
         self.lines_to_report.entry(start_line)
-            .or_insert( HorizontalMarkerLine::default() )
+            .or_insert( CodeLine::default() )
             .mark_as_multi_line_start(start_col, sign, style);
 
         self.lines_to_report.entry(end_line)
-            .or_insert( HorizontalMarkerLine::default() )
+            .or_insert( CodeLine::default() )
             .mark_as_multi_line_end(end_col, sign, style, labels);
 
 
@@ -61,93 +66,189 @@ impl<'a> CodeReporter<'a> {
 }
 
 #[derive(Default)]
-struct MainDepth;
-
-struct SubMainDepth;
-
-struct SubDepth;
-
-#[derive(Default)]
-struct HorizontalMarkerLine<'a, Level> {
-    markers: Vec<Marker<'a>>,
-    phantom_data: PhantomData<Level>,
+struct CodeLine<'a> {
+    /// Map column indecies to markers on them
+    markers: HashMap<usize, (Marker<'a>, MarkerType<'a>)>,
 }
 
-impl<'a> HorizontalMarkerLine<'a, MainDepth> {
-
-    fn check_col_or_resize(&mut self, col: usize){
-
-        if col < self.markers.len() {
-            return;
-        }
-
-        self.markers.resize_with(
-            col,
-            || Marker { sign: ' ', style: Style::new(), typ: MarkerType::Reapeted }
+impl<'a> CodeLine<'a> {
+    
+    fn mark_as_one_line(&mut self, start_col: usize, end_col: usize, sign: char, style: Style, labels: &'a [&'a str]) {
+        let marker = Marker { sign: MarkerSign::Char(sign), style: style };
+        self.markers.insert(
+            start_col,
+            (marker, MarkerType::OneLineStart { end_col: end_col, labels: labels } )
         );
     }
+    
+    fn mark_as_multi_line_start(&mut self, col: usize, sign: char, style: Style) {
+        let marker = Marker { sign: MarkerSign::Char(sign), style: style };
+        self.markers.insert(
+            col,
+            (marker, MarkerType::MultiLine(MultiLineMarkerType::Start) )
+        );
+    }
+    
+    fn mark_as_multi_line_end(&mut self, col: usize, sign: char, style: Style, labels: &'a [&'a str]) {
+        let marker = Marker { sign: MarkerSign::Char(sign), style: style };
+        self.markers.insert(
+            col,
+            (marker, MarkerType::MultiLine(MultiLineMarkerType::End { labels: labels }) )
+        );
+    }
+}
 
-    fn mark_in_line(&mut self, start_col: usize, end_col: usize, sign: char, style: Style, labels: &'a [&'a str]) {
+impl<'a> Display for CodeLine<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 
-        self.check_col_or_resize(end_col); // To expand until the end column
+        let mut painter = Painter::new(
+            Marker {sign: MarkerSign::Char(' '), style: Style::new()} // Default is space
+        );
 
-        let mut marker = Marker {
-            sign: sign,
-            style: style,
-            typ: MarkerType::OneLineStart { labels: labels }
-        };
+        let mut cols_rev = self.markers.keys().sorted().rev();
 
-        self.markers[start_col] = marker.clone(); // The start marker
+        let col = cols_rev.next().unwrap();
 
-        marker.typ = MarkerType::Reapeted; // Make after that repeated
-            
-        for i in start_col + 1 .. end_col {
-            self.markers[i] = marker.clone();
+        let (marker, marker_typ) = &self.markers[col];
+        match marker_typ {
+            MarkerType::Reapeted => todo!(),
+            MarkerType::OneLineStart { end_col, labels } => {
+                painter.move_right_by(*end_col).paint(
+                    marker.clone_with_str(labels[0])
+                );
+                for _ in 0 .. end_col - col {
+                    painter.move_left().paint(marker.clone());
+                }
+            },
+            MarkerType::MultiLine(MultiLineMarkerType::End { labels }) => {
+                painter.move_right_by(*col).paint(
+                    marker.clone_with_str(labels[0])
+                );
+                painter.move_left().paint(marker.clone());
+                for _ in 0 .. *col {
+                    painter.move_left().paint(marker.clone_with_char('_'));
+                }
+            },
+            MarkerType::MultiLine(MultiLineMarkerType::Start) => {
+                painter.move_right_by(*col).paint(
+                    marker.clone()
+                );
+            },
         }
 
+        let mut current_max_depth = 0;
+
+        write!(f, "{}", painter);
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test_code_line{
+    use std::{io::{self, Write}, process::Command};
+
+    use owo_colors::Style;
+
+    use super::CodeLine;
+
+    fn print_rtl(){
+        let output = Command::new("printf").arg(r#""\e[2 k""#).output().unwrap();
+        io::stdout().write_all(&output.stdout[1..output.stdout.len()-1]).unwrap();
     }
 
-    fn mark_as_multi_line_start(&mut self, col: usize, sign: char, style: Style) {
-
-        self.check_col_or_resize(col + 1); // To expand until the column after it
-
-        let marker = Marker {
-            sign: sign,
-            style: style,
-            typ: MarkerType::MultiLine(MultiLineMarkerType::Start),
-        };
-
-        self.markers[col] = marker;
-        
+    #[test]
+    fn test_one_line(){
+        print_rtl();
+        let line = "احجز متغير س = 555؛";
+        let mut code_line = CodeLine::default();
+        code_line.mark_as_one_line(
+            5, 
+            10, 
+            '^', 
+            Style::new().bold().yellow(),
+            &["من الممكن عدم جعل القيمة متغيرة"]
+        );
+        println!("{}\n{}", line, code_line)
     }
 
-    fn mark_as_multi_line_end(&mut self, col: usize, sign: char, style: Style, labels: &'a [&'a str]) {
-
-        self.check_col_or_resize(col); // To expand until the column
-
-        let marker = Marker {
-            sign: sign,
-            style: style,
-            typ: MarkerType::MultiLine(MultiLineMarkerType::End { labels: labels }),
-        };
-
-        self.markers[col - 1] = marker; // The column before
-
+    #[test]
+    fn test_multiline_start(){
+        print_rtl();
+        let line = "احجز متغير س = 555؛";
+        let mut code_line = CodeLine::default();
+        code_line.mark_as_multi_line_start(
+            5, 
+            '-', 
+            Style::new().bold().blue(),
+        );
+        println!("{}\n{}", line, code_line)
     }
 
+    #[test]
+    fn test_multiline_end(){
+        print_rtl();
+        let line = "احجز متغير س = 555؛";
+        let mut code_line = CodeLine::default();
+        code_line.mark_as_multi_line_end(
+            10, 
+            '^', 
+            Style::new().bold().red(),
+            &["من الممكن عدم جعل القيمة متغيرة"]
+        );
+        println!("{}\n{}", line, code_line)
+    }
 }
 
 #[derive(Clone)]
 struct Marker<'a> {
-    sign: char,
+    sign: MarkerSign<'a>,
     style: Style,
-    typ: MarkerType<'a>,
+}
+
+impl<'a> Marker<'a> {
+
+    #[inline]
+    fn new_char(ch: char, style: Style) -> Self {
+        Self { sign: MarkerSign::Char(ch), style: style }
+    }
+
+    #[inline]
+    fn new_str(s: &'a str, style: Style) -> Self {
+        Self { sign: MarkerSign::Str(s), style: style }
+    }
+
+    #[inline]
+    fn clone_with_char(&self, ch: char) -> Self {
+        Self::new_char(ch, self.style)
+    }
+
+    #[inline]
+    fn clone_with_str(&self, s: &'a str) -> Self {
+        Self::new_str(s, self.style)
+    }
+
+}
+
+impl<'a> Display for Marker<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.sign {
+            MarkerSign::Char(c) => write!(f, "{}", c.style(self.style)),
+            MarkerSign::Str(s) => write!(f, "{}", s.style(self.style)),
+        }
+    }
+}
+
+#[derive(Clone)]
+enum MarkerSign<'a> {
+    Char(char),
+    Str(&'a str)
 }
 
 #[derive(Clone)]
 enum MarkerType<'a> {
     Reapeted,
-    OneLineStart { labels: &'a [&'a str] },
+    OneLineStart { end_col: usize, labels: &'a [&'a str] },
     MultiLine(MultiLineMarkerType<'a>),
 }
 
@@ -207,13 +308,9 @@ mod tests {
         println!("  {} ", "|".bright_blue()); // Add empty line above
 
         for k in reporter.lines_to_report.keys().sorted() {
-            let v = &reporter.lines_to_report[k];
+            let marker_line = &reporter.lines_to_report[k];
             println!("{} {} {}", k.bright_blue(), "|".bright_blue(), reporter.files_lines[*k]);
-            print!("  {} ", "|".bright_blue());
-            for marker in v.markers.iter() {
-                print!("{}", marker.sign.style(marker.style))
-            }
-            println!()
+            println!("  {} {}", "|".bright_blue(), marker_line);
         }
     }
 }
